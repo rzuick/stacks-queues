52e0efc291c24b902e69ede9f41a97f5773c4edd
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-03 14:56:30 -0700

    rename Queue.rb -> Queue.py and setup for initial code

---
+++ stacks_queues/queue.py
@@ -0,0 +1,23 @@
+class Queue:
+
+    def __init__(self):
+        pass
+      
+
+    def enqueue(self, element):
+        pass
+
+    def dequeue(self):
+        pass
+
+    def front(self):
+        pass
+
+    def size(self):
+        pass
+
+    def empty(self):
+        pass
+
+    def __str__(self):
+        pass
