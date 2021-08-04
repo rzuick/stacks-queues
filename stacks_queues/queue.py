271c6f3dfc51277bb7ffcf40edbe68be5122c866
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-03 18:25:20 -0700

    add comments

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -18,20 +18,45 @@ class Queue:
       
 
     def enqueue(self, element):
+        """ Adds an element to the Queue
+            Raises a QueueFullException if all elements
+            In the store are occupied
+            returns None
+        """
         pass
 
     def dequeue(self):
+        """ Removes an element from the Queue
+            Raises a QueueEmptyException if 
+            The Queue is empty.
+            returns None
+        """
         pass
 
     def front(self):
+        """ Returns an element from the front
+            of the Queue and None if the Queue
+            is empty.  Does not remove anything.
+        """
         pass
         
 
     def size(self):
+        """ Returns the number of elements in
+            The Queue
+        """
         pass
 
     def empty(self):
+        """ Returns True if the Queue is empty
+            And False otherwise.
+        """
         pass
 
     def __str__(self):
+        """ Returns the Queue in String form like:
+            [3, 4, 7]
+            Starting with the front of the Queue and
+            ending with the rear of the Queue.
+        """
         pass
--- stacks_queues/stack.py
+++ stacks_queues/stack.py
@@ -9,13 +9,30 @@ class Stack:
         self.store = LinkedList()
 
     def push(self, element):
+        """ Adds an element to the top of the Stack.
+            Returns None
+        """
         pass
 
     def pop(self):
+        """ Removes an element from the top
+            Of the Stack
+            Raises a StackEmptyException if
+            The Stack is empty.
+            returns None
+        """
         pass
 
     def empty(self):
+        """ Returns True if the Stack is empty
+            And False otherwise
+        """
         pass
 
     def __str__(self):
-      pass
+        """ Returns the Stack in String form like:
+            [3, 4, 7]
+            Starting with the top of the Stack and
+            ending with the bottom of the Stack.
+        """
+        pass
