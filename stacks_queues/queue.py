5e6e5ff3f4e8115ec2156dddeb42e30f697977af
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2022-04-14 13:37:30 -0500

    Add queue method

--- stacks_queues/queue.py
+++ stacks_queues/queue.py
@@ -12,7 +12,7 @@ class Queue:
     def __init__(self):
         self.store = [None] * INITIAL_QUEUE_SIZE
         self.buffer_size = INITIAL_QUEUE_SIZE
-        self.front = -1
+        self.front = -1 # to show there is nothing in the queue yet
         self.rear = -1
         self.size = 0
       
@@ -24,6 +24,15 @@ class Queue:
             returns None
         """
         pass
+        if self.front == -1:
+            self.front = 0
+            self.rear = 0
+        elif self.size == self.buffer_size:
+            raise QueueFullException('Queue is full')
+
+        self.store[self.rear] = element
+        self.rear = (self.rear + 1) % self.buffer_size
+        self.size += 1
 
     def dequeue(self):
         """ Removes and returns an element from the Queue
@@ -31,6 +40,11 @@ class Queue:
             The Queue is empty.
         """
         pass
+        # Check if queue is empty to raise exception
+        # get element from front
+        # front moves to next index
+        # change size by 1
+        # check if it is empty
 
     def front(self):
         """ Returns an element from the front
@@ -44,13 +58,13 @@ class Queue:
         """ Returns the number of elements in
             The Queue
         """
-        pass
+        return self.size
 
     def empty(self):
         """ Returns True if the Queue is empty
             And False otherwise.
         """
-        pass
+        return self.size == 0
 
     def __str__(self):
         """ Returns the Queue in String form like:
